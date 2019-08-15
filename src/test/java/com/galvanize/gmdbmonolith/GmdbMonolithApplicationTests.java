07e046f582881c5564803858a8b95393550e1e5f
rlwing <rob@wingjr.com>
2019-08-15 12:39:20 -0700

    Add test properties with h2 db

--- src/test/java/com/galvanize/gmdbmonolith/GmdbMonolithApplicationTests.java
+++ src/test/java/com/galvanize/gmdbmonolith/GmdbMonolithApplicationTests.java
@@ -3,10 +3,12 @@ package com.galvanize.gmdbmonolith;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class GmdbMonolithApplicationTests {
 
 	@Test
